d3b993caec01cfdc4a64a2130310e89491693e6c
alxtim <alxtim10@gmail.com>
2023-10-10 21:19:08 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 const cors = require('cors');
-app.use(cors({credentials: true, origin: true}));
+app.use(cors());
 
 const trackerController = require("./tracker/tracker.controller");
 const usersController = require("./users/users.controller");
